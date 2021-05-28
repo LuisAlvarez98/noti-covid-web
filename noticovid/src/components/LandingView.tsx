fb8253566459e2046d32ca2ca5c2e8e80aa8c97e
LuisAlvarez98 <luisfalvarezsanchez@gmail.com>
2021-05-28 16:53:25 -0500

    Added API

--- noticovid/src/components/LandingView.tsx
+++ noticovid/src/components/LandingView.tsx
@@ -8,6 +8,8 @@ import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
 import { makeStyles } from "@material-ui/core/styles";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const TotalCasesText = styled.h1`
   color: green;
@@ -39,39 +41,75 @@ const useStyles = makeStyles({
   },
 });
 
+interface MunicipioType {
+  municipio: string;
+  confirmados: string;
+  defunciones: string;
+  sospechosos: string;
+}
 function createData(municipality: string, cases: number) {
   return { municipality, cases };
 }
 
-const rows = [
-  createData("Monterrey", 159),
-  createData("Guadalupe", 237),
-  createData("Apodaca", 262),
-  createData("San Nicolas", 305),
-  createData("Escobedo", 356),
-];
 const LandingView = () => {
   const classes = useStyles();
+  const [listaDatosMunicipios, setListaDatosMunicipios] = useState<
+    MunicipioType[]
+  >([]);
+  const [datosGenerales, setDatosGenerales] = useState({
+    confirmados: "",
+    defunciones: "",
+    sospechosos: "",
+    negativos: "",
+    activos: "",
+  });
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = async () => {
+    await axios
+      .get("https://noticovid.herokuapp.com/api/data")
+      .then((response) => {
+        setDatosGenerales(response.data.data.datosGenerales);
+        setListaDatosMunicipios(response.data.data.listaDatosMunicipios);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
+  const rows = [
+    createData("Monterrey", 159),
+    createData("Guadalupe", 237),
+    createData("Apodaca", 262),
+    createData("San Nicolas", 305),
+    createData("Escobedo", 356),
+  ];
   return (
     <div style={{ textAlign: "center" }}>
       <Container>
         <div>
-          <h1>Casos COVID19 en Nuevo Leon</h1>
+          <h1>Casos COVID19 en Nuevo León</h1>
           <h2>Casos totales:</h2>
-          <TotalCasesText>1,200,324</TotalCasesText>
+          <TotalCasesText>{datosGenerales.confirmados}</TotalCasesText>
         </div>
         <Grid container>
-          <Grid item xs={4}>
-            <h2>Casos activos:</h2>
-            <ActiveCasesText>1,200,324</ActiveCasesText>
+          <Grid item xs={12} md={3}>
+            <h4>Casos activos:</h4>
+            <ActiveCasesText>{datosGenerales.activos}</ActiveCasesText>
           </Grid>
-          <Grid item xs={4}>
-            <h2>Recuperados:</h2>
-            <RecoveredText>1,200,324</RecoveredText>
+          <Grid item xs={12} md={3}>
+            <h4>Negativos:</h4>
+            <RecoveredText>{datosGenerales.negativos}</RecoveredText>
           </Grid>
-          <Grid item xs={4}>
-            <h2>Defunciones:</h2>
-            <DeathsText>1,200,324</DeathsText>
+          <Grid item xs={12} md={3}>
+            <h4>Defunciones:</h4>
+            <DeathsText>{datosGenerales.defunciones}</DeathsText>
+          </Grid>
+          <Grid item xs={12} md={3}>
+            <h4>Sospechosos:</h4>
+            <DeathsText>{datosGenerales.sospechosos}</DeathsText>
           </Grid>
         </Grid>
         <h2 style={{ marginTop: "2.5em", textAlign: "left" }}>
@@ -86,22 +124,35 @@ const LandingView = () => {
             <TableHead>
               <TableRow>
                 <TableCell>Casos por municipio</TableCell>
-                <TableCell align="center">Casos</TableCell>
+                <TableCell align="center">Confirmados</TableCell>
+                <TableCell align="center">Defunciones</TableCell>
+                <TableCell align="center">Sospechosos</TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
-              {rows.map((row) => (
-                <TableRow key={row.municipality}>
+              {listaDatosMunicipios.map((mun: MunicipioType) => (
+                <TableRow key={mun.municipio}>
                   <TableCell component="th" scope="row">
-                    {row.municipality}
+                    {mun.municipio}
                   </TableCell>
-                  <TableCell align="center">{row.cases}</TableCell>
+                  <TableCell align="center">{mun.confirmados}</TableCell>
+                  <TableCell align="center">{mun.defunciones}</TableCell>
+                  <TableCell align="center">{mun.sospechosos}</TableCell>
                 </TableRow>
               ))}
             </TableBody>
           </Table>
         </TableContainer>
       </Container>
+      <p>
+        Fuente:{" "}
+        <a
+          href="http://www.gncys.com/covid19/covid19-estados.aspx?edoId=NL"
+          target="_new"
+        >
+          http://www.gncys.com/covid19/covid19-estados.aspx?edoId=NL
+        </a>{" "}
+      </p>
     </div>
   );
 };
